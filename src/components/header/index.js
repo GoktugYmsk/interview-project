9b30ce7301206c488ee44a93b2242fb95cc7f6c1
GoktugYmsk <gogtug033@outlook.com>
2023-06-04 12:28:29 +0300

    leftBar edited

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,26 @@
-import React from 'react'
-
-import './index.scss'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setInput } from '../configure/configure';
+import './index.scss';
 
 function Header() {
-  return (
-    <div className='header-container' >
-      <input placeholder='Search' />
-      <div className='header__basket' >
-        <p className='header__amount' ></p>
-        <p>icon</p>
-      </div>
-    </div>
-  )
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        const inputValue = e.target.value;
+        dispatch(setInput(inputValue));
+        console.log(inputValue);
+    };
+
+    return (
+        <div className='header-container'>
+            <input onChange={(e)=>handleChange(e)} placeholder='Search' />
+            <div className='header__basket'>
+                <p className='header__amount'></p>
+                <p>icon</p>
+            </div>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
