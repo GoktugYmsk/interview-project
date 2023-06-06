cdfae49468bca607af9c7f2227cfc128565bd10b
GoktugYmsk <gogtug033@outlook.com>
2023-06-06 22:57:21 +0300

    simple bug fixed & leftBar component added

--- src/components/header/basket/index.js
+++ src/components/header/basket/index.js
@@ -10,52 +10,61 @@ function Basket() {
     const wrapperRef = useRef(null);
     const [productCounts, setProductCounts] = useState({});
 
-    
     const amount = useSelector((state) => state.amountValue.amount);
     const handleDeleteProduct = (productId) => {
         const updatedProductList = selectedProductList.filter(
-          (product) => product.id !== productId
+            (product) => product.id !== productId
         );
         dispatch(setSelectedProductList(updatedProductList));
-      
+
         const deletedProductCount = productCounts[productId] || 0;
         dispatch(setAmount(amount - deletedProductCount));
-      };
+    };
 
-      
     const handleModalClose = () => {
         dispatch(setActive(false));
     };
-    
+
+    const handleCountChange = (productId, value) => {
+        const parsedValue = parseInt(value, 10) || 0;
+        setProductCounts((prevCounts) => ({
+            ...prevCounts,
+            [productId]: parsedValue,
+        }));
+        const previousCount = productCounts[productId] || 0;
+        const updatedAmount = amount - previousCount + parsedValue;
+        dispatch(setAmount(updatedAmount < 0 ? 0 : updatedAmount));
+    };
 
     const handleDecrementCount = (productId) => {
         const currentCount = productCounts[productId] || 0;
         const newCount = currentCount > 0 ? currentCount - 1 : 0;
-        setProductCounts(prevCounts => ({
+        setProductCounts((prevCounts) => ({
             ...prevCounts,
             [productId]: newCount,
         }));
-        dispatch(setAmount(amount - 1)); 
+        dispatch(setAmount(amount - 1 < 0 ? 0 : amount - 1));
     };
 
     const handleIncrementCount = (productId) => {
         const currentCount = productCounts[productId] || 0;
         const newCount = currentCount + 1;
-        setProductCounts(prevCounts => ({
+        setProductCounts((prevCounts) => ({
             ...prevCounts,
             [productId]: newCount,
         }));
-        dispatch(setAmount(amount + 1)); 
+        dispatch(setAmount(amount + 1));
     };
+
     useEffect(() => {
         const initialProductCounts = {};
         selectedProductList.forEach((product) => {
-          const existingCount = productCounts[product.id] || 0;
-          initialProductCounts[product.id] = existingCount > 0 ? existingCount : 1;
+            const existingCount = productCounts[product.id] || 0;
+            initialProductCounts[product.id] = existingCount > 0 ? existingCount : 1;
         });
         setProductCounts(initialProductCounts);
-      }, [selectedProductList]);
-      
+    }, [selectedProductList]);
+
     useEffect(() => {
         function handleClickOutside(event) {
             if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
@@ -83,7 +92,15 @@ function Basket() {
                                         <p>{product.title}</p>
                                     </div>
                                     <div className="basket-modal__list-alt">
-                                        <p className="basket-modal__list-amount">Adet: {productCounts[product.id] || 0}</p>
+                                        <p className="basket-modal__list-amount">
+                                            Adet:{' '}
+                                            <input
+                                                type="number"
+                                                min="0"
+                                                value={productCounts[product.id] || 0}
+                                                onChange={(e) => handleCountChange(product.id, e.target.value)}
+                                            />
+                                        </p>
                                         <div className="count-controls">
                                             <button onClick={() => handleDecrementCount(product.id)}>-</button>
                                             <button onClick={() => handleIncrementCount(product.id)}>+</button>
