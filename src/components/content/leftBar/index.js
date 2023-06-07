5fa49c5772e880f63f7b0efab4f51e686a5d28c5
GoktugYmsk <gogtug033@outlook.com>
2023-06-07 09:54:35 +0300

    simple bug fixed

--- src/components/content/leftBar/index.js
+++ src/components/content/leftBar/index.js
@@ -1,53 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import data from '../../../assets/data';
+import { Row, Col } from 'react-bootstrap';
 
 function LeftBar({ setList }) {
-    const [selectedCategories, setSelectedCategories] = useState([]);
-    const input = useSelector((state) => state.inputValue.input);
-    const uniqueCategories = [...new Set(data.map((product) => product.category))];
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const input = useSelector((state) => state.inputValue.input);
+  const productCateories = [...new Set(data.map((product) => product.category))];
 
-    const filterProducts = (category) => {
-        const categoryIndex = selectedCategories.indexOf(category);
-        let updatedCategories = [...selectedCategories];
-        if (categoryIndex > -1) {
-            updatedCategories.splice(categoryIndex, 1);
-        } else {
-            updatedCategories.push(category);
-        }
-        setSelectedCategories(updatedCategories);
+  const filterProducts = (category) => {
+    const categoryIndex = selectedCategories.indexOf(category);
+    let updatedCategories = [...selectedCategories];
+    if (categoryIndex > -1) {
+      updatedCategories.splice(categoryIndex, 1);
+    } else {
+      updatedCategories.push(category);
+    }
+    setSelectedCategories(updatedCategories);
 
-        if (updatedCategories.length > 0) {
-            const filteredList = data.filter(
-                (product) =>
-                    updatedCategories.includes(product.category) &&
-                    product.title.toLowerCase().includes(input.toLowerCase())
-            );
-            setList(filteredList);
-        } else {
-            setList(data);
-        }
-    };
+    if (updatedCategories.length > 0) {
+      const filteredList = data.filter(
+        (product) =>
+          updatedCategories.includes(product.category) &&
+          product.title.toLowerCase().includes(input.toLowerCase())
+      );
+      setList(filteredList);
+    } else {
+      setList(data);
+    }
+  };
 
-    return (
-        <div className="leftBar__container">
-            <div className='leftBar__container-box' >
-                <h3>Product Assortment</h3>
-                <ul>
-                    {uniqueCategories.map((option, index) => (
-                        <li key={index} onClick={() => filterProducts(option)}>
-                            <input
-                                type="checkbox"
-                                checked={selectedCategories.includes(option)}
-                                readOnly
-                            />
-                            {option}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <Row>
+      <Col  >
+      <div className="leftBar__container">
+        <div className="leftBar__container-box">
+          <h3>Product Assortment</h3>
+          <ul>
+            {productCateories.map((option, index) => (
+              <li key={index} onClick={() => filterProducts(option)}>
+                <input
+                  type="checkbox"
+                  checked={selectedCategories.includes(option)}
+                  readOnly
+                />
+                {option}
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      </div>
+      </Col>
+    </Row>
+  );
 }
 
-export default LeftBar
+export default LeftBar;
